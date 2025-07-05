6a518c46af5ee53141444ca9ec514e9ca150b812
wahllord <wahllord@outlook.com>
2025-07-05 23:46:45 +0200

    fixed bug

--- Projects/Discord-bots/wiener-bot/wiener-bot.py
+++ Projects/Discord-bots/wiener-bot/wiener-bot.py
@@ -1,8 +1,12 @@
 import discord
 from discord.ext import commands
 
-# Create a bot instance with a command prefix
-bot = commands.Bot(command_prefix='!')
+# Define the intents your bot needs
+intents = discord.Intents.default()
+intents.message_content = True  # Required to read message content
+
+# Create a bot instance with a command prefix and intents
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Dictionary to store user floats
 user_floats = {}
